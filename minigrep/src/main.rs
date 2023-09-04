1b702aa1f4dc0050c2cebdd9090d6e703e4d1b6f
Piers Powlesland <pierspowlesland@gmail.com>
2023-09-04 10:55:59 +0100

    Refactored config parsing and added error handling

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,16 +1,39 @@
 use std::env;
 use std::fs;
+use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     // dbg!(args);
-    let query = &args[1];
-    let file_path = &args[2];
+    // let query = &args[1];
+    // let file_path = &args[2];
 
-    println!("Searching for {}", query);
-    println!("In file {}", file_path);
+    let conf = Config::build(&args).unwrap_or_else(|err| {
+        println!("Problem parsing arguments: {err}");
+        process::exit(1);
+    });
 
-    let contents = fs::read_to_string(file_path).expect("should have been able to read file");
+    println!("Searching for {}", conf.query);
+    println!("In file {}", conf.file_path);
+
+    let contents = fs::read_to_string(conf.file_path).expect("should have been able to read file");
 
     println!("With text \n{}", contents);
+}
+
+struct Config {
+    query: String,
+    file_path: String,
+}
+
+impl Config {
+    fn build(args: &[String]) -> Result<Config, &'static str> {
+        if args.len() < 3 {
+            return Err("not enough arguments");
+        }
+        Ok(Config {
+            query: args[1].clone(),
+            file_path: args[2].clone(),
+        })
+    }
 }
