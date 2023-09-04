6f4f851b97607d33f7c210ad5fa57fd96cefaeef
Piers Powlesland <pierspowlesland@gmail.com>
2023-09-04 11:17:33 +0100

    Extract functionality to lib

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -1,15 +1,9 @@
 use std::env;
-use std::error::Error;
-use std::fs;
 use std::process;
 
 fn main() {
     let args: Vec<String> = env::args().collect();
-    // dbg!(args);
-    // let query = &args[1];
-    // let file_path = &args[2];
-
-    let conf = Config::build(&args).unwrap_or_else(|err| {
+    let conf = minigrep::Config::build(&args).unwrap_or_else(|err| {
         println!("Problem parsing arguments: {err}");
         process::exit(1);
     });
@@ -17,32 +11,8 @@ fn main() {
     println!("Searching for {}", conf.query);
     println!("In file {}", conf.file_path);
 
-    if let Err(e) = run(conf) {
+    if let Err(e) = minigrep::run(conf) {
         println!("Application error: {e}");
         process::exit(1);
-    }
-}
-
-fn run(config: Config) -> Result<(), Box<dyn Error>> {
-    let contents = fs::read_to_string(config.file_path)?;
-
-    println!("With text:\n{contents}");
-    Ok(())
-}
-
-struct Config {
-    query: String,
-    file_path: String,
-}
-
-impl Config {
-    fn build(args: &[String]) -> Result<Config, &'static str> {
-        if args.len() < 3 {
-            return Err("not enough arguments");
-        }
-        Ok(Config {
-            query: args[1].clone(),
-            file_path: args[2].clone(),
-        })
     }
 }
