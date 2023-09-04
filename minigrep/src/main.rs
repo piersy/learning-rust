6341ccff092262d5914a64e8f622f5a2dbb7cb92
Piers Powlesland <pierspowlesland@gmail.com>
2023-09-04 16:03:20 +0100

    Use iterators in place of loops

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -2,8 +2,13 @@ use std::env;
 use std::process;
 
 fn main() {
-    let args: Vec<String> = env::args().collect();
-    let conf = minigrep::Config::build(&args).unwrap_or_else(|err| {
+    // let args: Vec<String> = env::args().collect();
+    // let conf = minigrep::Config::build(&args).unwrap_or_else(|err| {
+    //     eprintln!("Problem parsing arguments: {err}");
+    //     process::exit(1);
+    // });
+
+    let conf = minigrep::Config::build(env::args()).unwrap_or_else(|err| {
         eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
