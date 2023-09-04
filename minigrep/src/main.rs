f673286aaebc6edb0c2a7eae79d6a8e650d47921
Piers Powlesland <pierspowlesland@gmail.com>
2023-09-04 13:37:10 +0100

    Print errors to stderr

--- minigrep/src/main.rs
+++ minigrep/src/main.rs
@@ -4,15 +4,12 @@ use std::process;
 fn main() {
     let args: Vec<String> = env::args().collect();
     let conf = minigrep::Config::build(&args).unwrap_or_else(|err| {
-        println!("Problem parsing arguments: {err}");
+        eprintln!("Problem parsing arguments: {err}");
         process::exit(1);
     });
 
-    println!("Searching for {}", conf.query);
-    println!("In file {}", conf.file_path);
-
     if let Err(e) = minigrep::run(conf) {
-        println!("Application error: {e}");
+        eprintln!("Application error: {e}");
         process::exit(1);
     }
 }
